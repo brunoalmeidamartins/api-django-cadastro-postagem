92a3a7641e42c970f730f7bbe7c23fe3c2b3f143
Bruno Martins <sgtbrunoalmeida@gmail.com>
2020-08-16 13:45:55 -0300

    Add rota de criação de usuário

--- django_rest/api/urls.py
+++ django_rest/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import postagem_view
+from .views import postagem_view, usuario_view
 
 urlpatterns = [
     path('postagens/', postagem_view.PostagemList.as_view(), name='postagem-list'),
     path('postagens/<int:id>', postagem_view.PostagemDetalhes.as_view(), name='postagem-detalhes'),
+    path('usuarios/', usuario_view.UsuarioList.as_view(), name='usuario-list'),
 ]
